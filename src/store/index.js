4fcdbc2761712fa75d6c1fea86c0295f480da657
Cizz22 <auliacisatra14@gmail.com>
2021-06-15 21:26:47 +0700

    Add Category component

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,15 @@ import auth from './modules/auth'
 import donation from './modules/donation'
 import profile from './modules/profile'
 import slider from './modules/slider'
-
+import category from './modules/category'
 
 const store = createStore({
     modules:{
         auth,
         donation,
         profile,
-        slider
+        slider,
+        category
     }
 })
 
